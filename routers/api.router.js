c2f2cb2e72bacf38d779dcaff0d27ddba974a20f
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-04-02 18:51:06 +0200

    Ajout des méthodes CRUD

--- routers/api.router.js
+++ routers/api.router.js
@@ -16,12 +16,45 @@ Routes definition
         
         // Set route fonctions
         routes(){
-            
-            
-            router.get('/', (req, res) => {
-                return res.json({ msg: 'Hello API' })
-            })
-            
+            /* 
+            CRUD: Create route 
+            */
+                router.post('/', (req, res) => {
+                    return res.json({ msg: 'CRUD: Create route' })
+                })
+            //
+
+            /* 
+            CRUD: Read all route 
+            */
+                router.get('/', (req, res) => {
+                    return res.json({ msg: 'CRUD: Read all route' })
+                })
+            //
+
+            /* 
+            CRUD: Read one route
+            */
+                router.get('/:id', (req, res) => {
+                    return res.json({ msg: 'CRUD: Read one route', id: req.params.id })
+                })
+            //
+
+            /* 
+            CRUD: Update route 
+            */
+                router.put('/:id', (req, res) => {
+                    return res.json({ msg: 'CRUD: Update route', id: req.params.id })
+                })
+            //
+
+            /* 
+            CRUD: Delete route 
+            */
+                router.delete('/:id', (req, res) => {
+                    return res.json({ msg: 'CRUD: Update route', id: req.params.id })
+                })
+            //            
         };
 
         // Start router
