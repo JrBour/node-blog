2fc76b11635648e8921056cce1d5d6bb8b94c432
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-04-23 11:58:37 +0200

    Mise à jour du .env.dist

--- services/mysql.class.js
+++ services/mysql.class.js
@@ -9,7 +9,7 @@ class MYSQLClass {
             port     :  process.env.MYSQL_PORT,
             user     : process.env.MYSQL_USER,
             password : process.env.MYSQL_PASS,
-            database : process.env.MYSQL_SCHEMA
+            database : process.env.MYSQL_DB
         })
     }
 
